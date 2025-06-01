a7ef547b98bd73917104208aa3813f5ded388bc8
ELISANGELA <90426066+elisangelaeleuterio@users.noreply.github.com>
2025-06-01 13:12:55 -0300

    Delete ecommerce-plantas directory

--- ecommerce-plantas/src/components/atoms/PriceTag.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/atoms/PriceTag.tsx
-
-
-const PriceTag = ({ value }: { value: number }) => {
-  return <span>{value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</span>;
-};
-
-export default PriceTag;
